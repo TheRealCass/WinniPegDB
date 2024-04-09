11f9cb5c1d2426ba4004db7860e99245262992cf
TheRealCass-HomeServer <60952504+TheRealCass@users.noreply.github.com>
2024-04-09 18:14:54 -0500

    brought everything back

---
+++ src/Database.java
@@ -0,0 +1,115 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+class MyDatabase {
+	private Connection connection;
+
+	public MyDatabase() {
+		try {
+			String url = "jdbc:sqlite:assets/storage.db";
+			// create a connection to the database
+			connection = DriverManager.getConnection(url);
+		} catch (SQLException e) {
+			e.printStackTrace(System.out);
+		}
+
+	}
+
+	//1
+	public void commandOne(String address) { 
+	
+		String sql =    "SELECT " +
+							"a.Ward AS Ward, " +
+							"c.Person AS Councillor, " +
+							"c.Phone AS Phone " + 
+						"FROM " +  
+							"Addresses a " + 
+						"JOIN " +  
+							"Council c " + 
+						"ON " +  
+							"a.Ward = c.WardName " + 
+						"WHERE " +  
+							"LOWER(a.FullAddress) = LOWER(?) " + 
+						"GROUP BY " + 
+							"Councillor";
+        try {
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+
+			//using our wildcard to set variables in sql statemet
+            pstmt.setString(1, address);
+
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+				String ward = rs.getString("Ward");
+				String councillor = rs.getString("Councillor");
+				String phoneNumber = rs.getString("Phone");
+				String msg = councillor + " is in charge of " + ward + " which covers " + address + ". Their contact info is " + phoneNumber + ".";
+                System.out.println( msg);
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+	}
+
+	//2
+	public void commandTwo() {
+        
+
+	}
+
+	//3
+	public void commandThree() {
+		
+		
+
+	}
+
+
+	//4
+	public void commandFour() {
+		
+	}
+
+	//5
+	public void commandFive() {
+
+		String sql =    "SELECT " +
+							"ServiceRequest AS Event, " + 
+							"Person AS Councillor, " + 
+							"ward, year " + 
+						"FROM " + 
+							"ServiceCalls " +
+						"JOIN " + 
+							"Council " + 
+						"ON " +
+							"ServiceCalls.Ward = Council.WardName " + 
+						"GROUP BY " + 
+							"ServiceRequest";
+        try {
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+
+			System.out.println("ward\t\tservice request\t\tcouncillor in charge\t\tyear of office");
+            while (rs.next()) {
+				String serviceRequest = rs.getString("Event");
+				String councillor = rs.getString("Councillor");
+				String ward = rs.getString("ward");
+				String year = rs.getString("year");
+				String msg = ward + " has the most complains regarding " + serviceRequest + ". " + councillor + " was in charge of this ward in " + year + ".";
+				//String msg = ward + "\t\t" + serviceRequest + "\t\t" + councillor + "\t\t" + year;
+                System.out.println( msg);
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+			
+	}
+
+	
+}
---
+++ src/GroupProject.java
@@ -0,0 +1,103 @@
+import java.sql.Connection;
+import java.util.Scanner;
+
+public class GroupProject {
+	static Connection connection;
+	private static final String COMMAND_ONE = "a";
+	private static final String COMMAND_TWO = "b";
+	private static final String COMMAND_THREE = "c";
+	private static final String COMMAND_FOUR = "d";
+	private static final String COMMAND_FIVE = "e";
+	private static final String QUIT_COMMAND = "q";
+	private static final String HELP_COMMAND = "h";
+	private static final String INTRO_LINE = "Welcome! Type h for help. ";
+	private static final String CURSOR = "db > ";
+	private static final String DBMS_NAME = "City of Winnipeg DB";
+
+	public static void main(String[] args) throws Exception {
+
+		// startup sequence
+		MyDatabase db = new MyDatabase();
+		runConsole(db);
+
+		System.out.println("Exiting...");
+	}
+
+	public static void runConsole(MyDatabase db) {
+
+		Scanner console = new Scanner(System.in);
+		System.out.print(INTRO_LINE);
+		System.out.print(CURSOR);
+		String line = console.nextLine();
+		String[] parts;
+		String arg = "";
+
+		while (line != null && !line.equals(QUIT_COMMAND)) {
+			parts = line.split("\\s+");
+			if (line.indexOf(" ") > 0){
+				arg = line.substring(line.indexOf(" ")).trim();
+			}
+
+			if (parts[0].equals(HELP_COMMAND)) {
+				printHelp();
+
+			} else if (parts[0].equals(COMMAND_ONE)) {
+				
+				if (parts.length >= 2) {
+					db.commandOne(arg);
+				} else {
+					System.out.println("Require an argument for this command");
+				}
+			
+			} else if (parts[0].equals(COMMAND_TWO)) {
+
+
+			
+			} else if (parts[0].equals(COMMAND_THREE)) {
+
+
+			
+			} else if (parts[0].equals(COMMAND_FOUR)) {
+
+
+			
+			} else if (parts[0].equals(COMMAND_FIVE)) {
+				db.commandFive();
+			} else{
+				System.out.println("Read the help with h, or find help somewhere else.");
+			}
+
+			System.out.print(CURSOR);
+			line = console.nextLine();
+		}
+
+		console.close();
+	}
+
+	private static void printHelp() {
+		String commandOneDiscription = "Given a address, what ward does it belong to and who (councillor) is in charge of said ward, and how do i get in contact with him.";
+		commandOneDiscription += "\n\tEscalating event at a certain location? Lets you contact the councillor in charge to help resolve conflict.";
+
+		String commandTwoDiscription = "";
+
+		String commandThreeDiscription = "";
+
+		String commandFourDiscription = "";
+		
+		String commandFiveDiscription = "What were the most service requests in the city, and which ward they occured, and which councillors were in charge of said ward (and in which year they were in charge).";
+		commandFiveDiscription += "\n\tEssentially, lets you take a look at what were the dificianncies of the councillor and in wards these dificiancies occured. It also shows which year the councillors where in charge";
+
+		System.out.println(DBMS_NAME);
+		System.out.println("Commands:");
+		System.out.println(COMMAND_ONE + " <desired address to look up> - " + commandOneDiscription);
+		System.out.println(COMMAND_TWO + " - " + commandTwoDiscription);
+		System.out.println(COMMAND_THREE + " - " + commandThreeDiscription);
+		System.out.println(COMMAND_FOUR + " - " + commandFourDiscription);
+		System.out.println(COMMAND_FIVE + " - " + commandFiveDiscription);
+		System.out.println(HELP_COMMAND + " - Get help");
+		System.out.println(QUIT_COMMAND + " - Exit the program");
+		System.out.println("---- end help ----- ");
+	}
+
+}
+
